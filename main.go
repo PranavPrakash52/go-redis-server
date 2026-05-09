35cdf97de9c1144e83eb3bcf2cd93a910bfda758
Pranav <pranav@Pranavs-MBP.mynet>
2026-05-09 20:43:18 +0100

    Initial commit: Redis server implementation in Go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"go-redis-server/config"
+	"go-redis-server/server"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the dice server")
+	flag.IntVar(&config.Port, "port", 7379, "port for the dice server")
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+	log.Println("rolling the dice 🎲")
+	server.RunSyncTCPServer()
+}
