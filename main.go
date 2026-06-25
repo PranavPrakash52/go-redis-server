3450239b88af3734043c0854cc1cc1e1ecc54f35
Pranav <pranav@Pranavs-MacBook-Pro.local>
2026-06-25 14:47:52 +0100

    chore: remove DiceDB references from config and startup

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"go-redis-server/config"
 	"go-redis-server/server"
 	"log"
 )
 
 func setupFlags() {
-	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the dice server")
-	flag.IntVar(&config.Port, "port", 7379, "port for the dice server")
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the go-redis-server")
+	flag.IntVar(&config.Port, "port", 6379, "port for the go-redis-server")
 	flag.Parse()
 }
 
 func main() {
 	setupFlags()
-	log.Println("***** genesis *****")
+	log.Println("starting go-redis-server on", fmt.Sprintf("%s:%d", config.Host, config.Port))
 	err := server.RunAsyncTCPServerDarwin()
 	if err != nil {
 		log.Println("err", err)
